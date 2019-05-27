bd298dd6e27d8e6c28b6cbda0628ba1af520a5bb
Florian Wolters <florian@florian-wolters.de>
2019-05-27 08:35:20 +0200

    Refactor file opening and disable output temporarily

--- DLRS2iTelex.pl
+++ DLRS2iTelex.pl
@@ -2,33 +2,45 @@
 
 use strict;
 use warnings;
+use DateTime;
 use Device::SerialPort;
 
 $| = 1;
 
-my $port = Device::SerialPort->new("/dev/ttyUSB0");
+# Open file with current Deutschlandrundspruch by DARC
 
-$port->baudrate(9600); # Configure this to match your device
-$port->databits(8);
-$port->parity("none");
-$port->stopbits(1);
+my $dt = DateTime->now;
+my $year = $dt->year;
+my $week = $dt->week_number;
 
-$port->write("\x1");
-sleep(1);
+my $file = "dlrs/dlrs".$year.$week.".txt";
 
-$port->write("05");
-sleep(1);
+open(my $fh, '<', $file) # always use a variable here containing filename
+	or die "Unable to open file $file, $!";
 
-open(my $yourhandle, '<', 'dlrs201919.txt') # always use a variable here containing filename
-    or die "Unable to open file, $!";
-
-#$port->write("Hello world\r\n");
-
-while (<$yourhandle>) {
- $port->write("$_");
- sleep(10);
+while (<$fh>) {
+	print $_;
 }
 
-$port->write("\x13");
+# my $port = Device::SerialPort->new("/dev/ttyUSB0");
+# 
+# $port->baudrate(9600); # Configure this to match your device
+# $port->databits(8);
+# $port->parity("none");
+# $port->stopbits(1);
+# 
+# $port->write("\x1");
+# sleep(1);
+# 
+# $port->write("05");
+# sleep(1);
+# 
+# 
+# while (<$yourhandle>) {
+#  $port->write("$_");
+#  sleep(10);
+# }
+# 
+# $port->write("\x13");
 
 exit 0;
