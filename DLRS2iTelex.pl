bec9b48a3b0b35de66e7575224c8d009ff8d99c9
Florian Wolters <florian@florian-wolters.de>
2019-05-28 17:06:05 +0200

    Rewrap text and add delay for output

--- DLRS2iTelex.pl
+++ DLRS2iTelex.pl
@@ -3,7 +3,16 @@
 use strict;
 use warnings;
 use DateTime;
+use Text::Wrap;
 use Device::SerialPort;
+
+
+my $dlrs_dir = "/home/itelex/dlrs";
+my $itelex_port = "/dev/iTelex";
+my $itelex_extension = 05;
+my $linewidth = 68;
+
+$Text::Wrap::columns = $linewidth;
 
 $| = 1;
 
@@ -13,34 +22,55 @@ my $dt = DateTime->now;
 my $year = $dt->year;
 my $week = $dt->week_number;
 
-my $file = "dlrs/dlrs".$year.$week.".txt";
+my $file = $dlrs_dir."/dlrs".$year.$week.".txt";
 
-open(my $fh, '<', $file) # always use a variable here containing filename
-	or die "Unable to open file $file, $!";
+open(my $ifh, '<', $file) # always use a variable here containing filename
+   or die "Unable to open file $file, $!";
 
-while (<$fh>) {
-	print $_;
+$file =~ s/\.txt/_wrapped.txt/;
+
+open(my $ofh, '>', $file) # always use a variable here containing filename
+   or die "Unable to open file $file, $!";
+
+while (<$ifh>) {
+   my $line = $_;
+   chomp($line);
+   if (length($line) > $linewidth) {
+      print $ofh wrap('', '', $line) . "\n";
+   } else {
+      print $ofh $line."\n";
+   }
 }
 
-# my $port = Device::SerialPort->new("/dev/ttyUSB0");
-# 
-# $port->baudrate(9600); # Configure this to match your device
-# $port->databits(8);
-# $port->parity("none");
-# $port->stopbits(1);
-# 
-# $port->write("\x1");
-# sleep(1);
-# 
-# $port->write("05");
-# sleep(1);
-# 
-# 
-# while (<$yourhandle>) {
-#  $port->write("$_");
-#  sleep(10);
-# }
-# 
-# $port->write("\x13");
+close ($ifh);
+close ($ofh);
+
+# Open iTelex Device
+my $port = Device::SerialPort->new($itelex_port)
+   or die "Unable to open port: $itelex_port, $!";
+
+$port->baudrate(9600);
+$port->databits(8);
+$port->parity("none");
+$port->stopbits(1);
+
+# Dial iTelex extension
+$port->write("\x1");
+sleep(1);
+
+$port->write("05");
+sleep(1);
+
+open($ifh, '<', $file) # always use a variable here containing filename
+   or die "Unable to open file $file, $!";
+
+while (<$ifh>) {
+ $port->write("$_");
+ my $delay = length($_) * 0.2;
+ sleep($delay);
+}
+ 
+$port->write("\x13");
+sleep(1);
 
 exit 0;
